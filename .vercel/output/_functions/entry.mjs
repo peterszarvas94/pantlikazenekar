e87e4c770b900b94a129c62dde4602bb4090a2d0
Peter Szarvas <contact@peterszarvas.hu>
2025-07-22 15:08:06 +0200

    fix: assets

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_CiAGPA7O.mjs';
+import { manifest } from './manifest_BCMKyt6j.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "620cb96c-dbfc-4245-8cb7-e9ab85c408b6",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
---
+++ .vercel/output/_functions/pages/index.astro.mjs
@@ -0,0 +1,145 @@
+import { c as createComponent, m as maybeRenderHead, r as renderTemplate, b as renderHead, e as renderComponent, f as addAttribute } from '../chunks/astro/server_DZSa-pY3.mjs';
+import 'kleur/colors';
+import '../chunks/index_BRwviSIQ.mjs';
+import { $ as $$Image } from '../chunks/_astro_assets_faJgvrTI.mjs';
+import 'clsx';
+/* empty css                                 */
+export { renderers } from '../renderers.mjs';
+
+const $$Download = createComponent(($$result, $$props, $$slots) => {
+  return renderTemplate`${maybeRenderHead()}<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 15.575q-.2 0-.375-.062T11.3 15.3l-3.6-3.6q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L11 12.15V5q0-.425.288-.713T12 4q.425 0 .713.288T13 5v7.15l1.875-1.875q.3-.3.713-.288t.712.313q.275.3.288.7t-.288.7l-3.6 3.6q-.15.15-.325.213t-.375.062ZM6 20q-.825 0-1.413-.588T4 18v-2q0-.425.288-.713T5 15q.425 0 .713.288T6 16v2h12v-2q0-.425.288-.713T19 15q.425 0 .713.288T20 16v2q0 .825-.588 1.413T18 20H6Z"></path></svg>`;
+}, "/Users/szarvaspeter/projects/pantlika/src/icons/Download.astro", void 0);
+
+const Logo = new Proxy({"src":"/_astro/logo.BJTCp4Ms.png","width":960,"height":570,"format":"png"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/szarvaspeter/projects/pantlika/src/images/logo.png";
+							}
+							
+							return target[name];
+						}
+					});
+
+const Borito = new Proxy({"src":"/_astro/borito.BR4NWjZs.jpg","width":2048,"height":1365,"format":"jpg"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/szarvaspeter/projects/pantlika/src/images/borito.jpg";
+							}
+							
+							return target[name];
+						}
+					});
+
+const Sav = new Proxy({"src":"/_astro/sav.2UIavYX1.png","width":583,"height":126,"format":"png"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/szarvaspeter/projects/pantlika/src/images/sav.png";
+							}
+							
+							return target[name];
+						}
+					});
+
+const Csoori1 = new Proxy({"src":"/_astro/1.EyJCQSaq.png","width":225,"height":225,"format":"png"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/szarvaspeter/projects/pantlika/src/images/csoori/1.png";
+							}
+							
+							return target[name];
+						}
+					});
+
+const Csoori2 = new Proxy({"src":"/_astro/2.BLeDsuDY.png","width":271,"height":186,"format":"png"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/szarvaspeter/projects/pantlika/src/images/csoori/2.png";
+							}
+							
+							return target[name];
+						}
+					});
+
+const Studio1 = new Proxy({"src":"/_astro/studio1.BIKv7fn2.jpg","width":1599,"height":1200,"format":"jpg"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/szarvaspeter/projects/pantlika/src/images/csoori/studio1.jpg";
+							}
+							
+							return target[name];
+						}
+					});
+
+const Studio2 = new Proxy({"src":"/_astro/studio2.BcodI5LN.jpg","width":1599,"height":1200,"format":"jpg"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/szarvaspeter/projects/pantlika/src/images/csoori/studio2.jpg";
+							}
+							
+							return target[name];
+						}
+					});
+
+const Szerzodes = "/_astro/P%C3%A1ntlika_Zenekar_szerz%C5%91d%C3%A9s._doGK04n.pdf";
+
+const $$Index = createComponent(($$result, $$props, $$slots) => {
+  return renderTemplate`<!--
+	Author name: Szarvas Péter
+	Author email: peterszarvas94@gmail.com
+	Release date: 2019. 04. 25.
+	Repo: https://github.com/peterszarvas94/pantlikazenekar
+	Authos site: https://peterszarvas.hu  
+	Ajándék a híres bátai Pántlika Zenekarnak. :)
+--><html lang="hu"> <head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Pántlika Zenekar</title>${renderHead()}</head> <body> <main> <!-- LOGO --> ${renderComponent($$result, "Image", $$Image, { "alt": "logo", "class": "logo", "src": Logo })} <!--SOCIAL LINKS--> <ul class="social_links"> <li> <a target="_blank" href="https://www.facebook.com/pantlikazenekar/" title="Facebook"> <i class="icon-facebook-squared"></i> </a> </li> <li> <a target="_blank" href="https://www.instagram.com/pantlikatamburazenekar/" title="Instagram"> <i class="icon-instagram-1"></i> </a> </li> <li> <a target="_blank" href="mailto:pantlikazenekar@gmail.com" title="Email"> <i class="icon-mail-squared"></i> </a> </li> <li> <a target="_blank" href="tel:+36707785049" title="Telefon"> <i class="icon-phone-squared"></i> </a> </li> <li> <a target="_blank" href="https://www.youtube.com/playlist?list=PLK2TbLF0FDt9YC-9f2SIh6tZa59ygKvMy" href="" title="YouTube"> <i class="icon-youtube-squared"></i> </a> </li> </ul> <!--YOUTUBE PLAYER--> <div class="yt_outer"> <div class="yt_inner"> <iframe class="yt_iframe" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLK2TbLF0FDt9YC-9f2SIh6tZa59ygKvMy&index=1" style="border: 0px;" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </div> </div> <!--CONTRACT--> <a class="contract"${addAttribute(Szerzodes, "href")} download="Pántlika_Zenekar_szerződés.pdf" target="_black" rel="noopener noreferrer"> <div>Műsorszerződés</div> ${renderComponent($$result, "Download", $$Download, {})} </a> ${renderComponent($$result, "Image", $$Image, { "alt": "Sav", "class": "sav", "src": Sav })} <!-- ABOUT --> <div class="about"> <h2 class="about_title">Rólunk</h2> <p>
+Zenekarunk 2010-ben alakult meg Bátán helyi fiatalokból. Az alapítók
+          közül már csak Bence és Barnabás maradt. A jelenlegi felállás 2018 óta
+          zenél együtt. Nagyrészt Bátáról származunk, a Sárköz egyik kiemelkedő
+          hagyományőrző falujából. Ezáltal fontosnak tartjuk, hogy e falu zenei
+          hagyományait, a tamburahagyományt ápoljuk. Repertoárunk gerincét a
+          külső- és belső-sárközi népzene alkotja, de szívesen játszunk
+          kalocsai, szatmári, őrkői, galgamenti és más dallamokat is.
+</p> </div> ${renderComponent($$result, "Image", $$Image, { "alt": "Sav", "class": "sav", "src": Sav })} <!-- MEMBERS --> <div class="members"> <div class="members_parent"> <h2 class="members_title">Tagok</h2> <ul class="members_list"> <li>(jobbról balra)</li> <li>Szabó Barnabás - prímtambura</li> <li>Szabó Péter Bence - tamburabrácsa</li> <li>Fehér Dániel - basszprím tambura</li> <li>Szabó Ádám - basszprím tambura</li> <li>Magdali Anna - ének</li> <li>Szarvas Péter - tamburabőgő</li> </ul> </div> ${renderComponent($$result, "Image", $$Image, { "class": "members_pic", "src": Borito, "alt": "" })} </div> ${renderComponent($$result, "Image", $$Image, { "alt": "Sav", "class": "sav", "src": Sav })} <!-- CSOORI --> <div class="csoori"> <div class="csoori-logo-container"> ${renderComponent($$result, "Image", $$Image, { "alt": "Csoori1", "class": "csoori-logo", "src": Csoori1 })} ${renderComponent($$result, "Image", $$Image, { "alt": "Csoori2", "class": "csoori-logo", "src": Csoori2 })} </div> <h2 class="csoori_title">Rólunk</h2> <p>
+2020-ban az Emberi Erőforrások Minisztériuma a Csoóri Sándor Program
+          keretein belül támogatta a zenekarunk működését. A támogatást a
+          következőkre fordítottuk:
+</p> <ul> <li>stúdiófelvétel</li> <li>CD borító elkészítése</li> <li>technikai eszközök beszerzése (mikrofonok)</li> <li>együttműködés szakmai mentorral</li> </ul> <div class="csoori-image-container"> ${renderComponent($$result, "Image", $$Image, { "alt": "Studio1", "class": "csoori-image", "src": Studio1 })} ${renderComponent($$result, "Image", $$Image, { "alt": "Studio2", "class": "csoori-image", "src": Studio2 })} </div> </div> ${renderComponent($$result, "Image", $$Image, { "alt": "Sav", "class": "sav", "src": Sav })} </main> <footer> <!-- AUTHOR --> <div class="author"> <p>© 2023 Szarvas Péter</p> <a href="https://peterszarvas.hu" target="_blank" rel="noopener noreferrer">
+peterszarvas.hu
+</a> <a href="mailto:contact@peterszarvas.hu" target="_blank" rel="noopener noreferrer">
+contact@peterszarvas.hu
+</a> <p>Minden jog fenntartva.</p> </div> </footer> </body></html>`;
+}, "/Users/szarvaspeter/projects/pantlika/src/pages/index.astro", void 0);
+
+const $$file = "/Users/szarvaspeter/projects/pantlika/src/pages/index.astro";
+const $$url = "";
+
+const _page = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	default: $$Index,
+	file: $$file,
+	url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+const page = () => _page;
+
+export { page };
